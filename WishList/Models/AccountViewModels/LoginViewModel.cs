b28f681db33fefd5645f6468daff925b1aa240a7
Mauricio Hidalgo <mhidalgoe@hotmail.com>
2018-11-03 00:23:15 -0700

    Completed AccountController

--- WishList/Models/AccountViewModels/LoginViewModel.cs
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -9,7 +9,7 @@ namespace WishList.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
