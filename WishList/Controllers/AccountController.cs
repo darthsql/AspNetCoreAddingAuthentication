7899263e5f48b44b21ccfaea9886a1ed14f72c67
Mauricio Hidalgo <mhidalgoe@hotmail.com>
2018-11-02 23:40:08 -0700

    Completed AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
-namespace WishList.Controllers
+namespace WishList.Controllers 
 {
-    public class AccountController
+    [Authorize]
+    public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
     }
 }
